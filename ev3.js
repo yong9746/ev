195d8b73858d018e33768185a22276d32ebc1397
yong9746 <yong9746@hotmail.com>
2024-08-02 00:47:05 +0800

    Add files via upload

---
+++ ev3.js
@@ -0,0 +1,113 @@
+const express = require('express');
+const app = express();
+const port = 8000;
+const puppeteer = require('puppeteer-core');
+
+const proxy = 'http://23.247.105.131:5195';
+const proxyUsername = 'msnmmayl';
+const proxyPassword = '626he4yucyln';
+
+app.get('/', async (req, res) => {
+    const targetUrl = req.query.url;
+    if (!targetUrl) {
+        return res.status(400).send('Missing url query parameter');
+    }
+
+
+    (async () => {
+        const browser = await puppeteer.connect({
+            browserWSEndpoint: `wss://production-sfo.browserless.io?&token=Qa46iM22tXieVp7bfe3b9970bc76061e0778f3b834&--proxy-server=http://104.223.227.145:6668`,
+        });
+
+        const page = await browser.newPage();
+
+        await page.authenticate({
+            username: 'msnmmayl',
+            password: '626he4yucyln',
+        });
+
+        // Set cookies
+        await page.setCookie({
+            name: '_elements_session_4',
+            value: 'RGxUZHY1UyszTjNHZCtONXIxTzhyNWZuS2tIak1PMnB1RURiY1crSEZDeWRlWGtjeHJGeGZvRjBmRUZTT2ZUd201bjFVamZFK0pLSzI0MFBYNjBnNFFSWmcyWVdaTi8vVmlZbnY5L1JBQWdaUzU5L1lvbjQvLzJrU1RJT3FYSEZrdnJWRFBQSFJ1M0RXNlhhd3RJdU9RWklDcmtzd2xzSlpwQ2s4elJmUEFReHNKRi94UjFYQTBaaUVoQkM2MHlqYWtBT1MveGtEelQzRERrSzVTTXpaWEJzTTVUakluR1hWTEoycmcyRDlsa2VVVG1McktqSFFjOXdJdVEweUJod05Vc1UyWGFLVEtnTktOK3Y3aUV5VmE0WHQ5b3FVbzQzem9iZ043eXJpUE1yZHROTlRTakRkQWxCblhXNUpMYkhKLzBwL2ZWNTVxOGJvVWpnQlpyVkdYOEJ1MTl2MDJRblQrNVdPQi9YdStISjBWREJWK3F4c0EzK2hKMEZ1elVjdWgwMUFMdWJITUJzMmwzSHVBbktXdz09LS0xYlpyYkZZaHlwV09wWW5wMDBTNUdnPT0%3D--66b7f6885fa8f7f6955761c72808483e37f037fc',
+            domain: '.elements.envato.com', // Adjust the domain to match the target site
+        });
+
+        await page.goto(targetUrl, { waitUntil: 'networkidle2' });
+
+        await page.waitForFunction(() =>
+            Array.from(document.querySelectorAll('button, a'))
+                .some(el => el.textContent.trim() === 'Accept all')
+        );
+
+        // Click the button with text 'Accept all'
+        await page.evaluate(() => {
+            const button = Array.from(document.querySelectorAll('button, a'))
+                .find(el => el.textContent.trim() === 'Accept all');
+            if (button) {
+                button.click();
+            }
+        });
+
+        // Wait for the button with text 'Let's create!' to be available
+        await page.waitForFunction(() =>
+            Array.from(document.querySelectorAll('button'))
+                .some(el => el.textContent.trim() === "Let's create!")
+        );
+
+        // Click the button with text 'Let's create!'
+        await page.evaluate(() => {
+            const button = Array.from(document.querySelectorAll('button'))
+                .find(el => el.textContent.trim() === "Let's create!");
+            if (button) {
+                button.click();
+            }
+        });
+
+        // Wait for the element containing the text to load
+        await page.waitForSelector('.woNBXVXX');
+
+        // Extract the text content
+        const text = await page.evaluate(() => {
+            return document.querySelector('.woNBXVXX').innerText;
+        });
+
+        console.log('Extracted Text:', text);
+        await page.keyboard.press('Escape');
+
+        // Wait for the button to be available in the DOM
+        await page.waitForSelector('.ncWzoxCr.WjwUaJcT.NWg5MVVe.METNYJBx');
+
+        // Click the button
+        await page.click('.ncWzoxCr.WjwUaJcT.NWg5MVVe.METNYJBx');
+
+        // Wait for the button to be available in the DOM
+        await page.waitForSelector('[data-testid="download-without-license-button"]');
+        console.log('Button clicked2!');
+        // Click the button
+        await page.click('[data-testid="download-without-license-button"]');
+
+        // Set up request interception
+        await page.setRequestInterception(true);
+
+        page.on('request', request => {
+            const url = request.url();
+            if (url.includes('elements-downloads.envatousercontent.com')) {
+                // Log the URL
+                console.log('Intercepted request URL:', url);
+                res.send(url);
+
+                // Abort the request
+                request.abort();
+            } else {
+                // Allow the request to continue
+                request.continue();
+            }
+        });
+        // await browser.close();
+    })();
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
