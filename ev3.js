80c304830d36201a0a09ec02ef11f46e045ffe24
yong9746 <yong9746@hotmail.com>
2024-08-29 10:51:48 +0800

    Update ev3.js

--- ev3.js
+++ ev3.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 8000;
 const puppeteer = require('puppeteer-core');
 
-const proxy = 'http://64.137.57.171:6180';
+const proxy = 'http://45.43.83.51:6334';
 const proxyUsername = 'msnmmayl';
 const proxyPassword = '626he4yucyln';
 
@@ -16,7 +16,7 @@ app.get('/', async (req, res) => {
 
     (async () => {
         const browser = await puppeteer.connect({
-            browserWSEndpoint: `wss://production-sfo.browserless.io?&token=Qk9twpkC3PV4rz1ab1e884e7835b6d83400e867069&--proxy-server=http://64.137.57.171:6180`,
+            browserWSEndpoint: `wss://production-sfo.browserless.io?&token=Qk9twpkC3PV4rz1ab1e884e7835b6d83400e867069&--proxy-server=http://45.43.83.51:6334`,
         });
 
         const page = await browser.newPage();
