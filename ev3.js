ef1ed0091f5f46d9526fa164659c1868c81d89f8
yong9746 <yong9746@hotmail.com>
2024-08-29 12:28:09 +0800

    Update ev3.js

--- ev3.js
+++ ev3.js
@@ -33,8 +33,8 @@ app.get('/', async (req, res) => {
             domain: '.elements.envato.com', // Adjust the domain to match the target site
         });
 
-        // await page.goto(targetUrl, { waitUntil: 'networkidle2' });
-        //     console.log('link Text:', targetUrl);
+        await page.goto(targetUrl, { waitUntil: 'networkidle2' });
+            console.log('link Text:', targetUrl);
         // await page.waitForFunction(() =>
         //     Array.from(document.querySelectorAll('button, a'))
         //         .some(el => el.textContent.trim() === 'Accept all')
@@ -50,19 +50,19 @@ app.get('/', async (req, res) => {
         // });
          console.log('link Text1:', targetUrl);
         // Wait for the button with text 'Let's create!' to be available
-        await page.waitForFunction(() =>
-            Array.from(document.querySelectorAll('button'))
-                .some(el => el.textContent.trim() === "Let's create!")
-        );
+        // await page.waitForFunction(() =>
+        //     Array.from(document.querySelectorAll('button'))
+        //         .some(el => el.textContent.trim() === "Let's create!")
+        // );
 
-        // Click the button with text 'Let's create!'
-        await page.evaluate(() => {
-            const button = Array.from(document.querySelectorAll('button'))
-                .find(el => el.textContent.trim() === "Let's create!");
-            if (button) {
-                button.click();
-            }
-        });
+        // // Click the button with text 'Let's create!'
+        // await page.evaluate(() => {
+        //     const button = Array.from(document.querySelectorAll('button'))
+        //         .find(el => el.textContent.trim() === "Let's create!");
+        //     if (button) {
+        //         button.click();
+        //     }
+        // });
  console.log('link Text2:', targetUrl);
         // Wait for the element containing the text to load
         await page.waitForSelector('.woNBXVXX');
